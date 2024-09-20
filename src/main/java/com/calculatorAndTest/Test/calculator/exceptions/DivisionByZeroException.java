25aef34e1fcbcc03748582208095e280204437f2
Zver <SvetlanaEvcheko@yandex.ru>
2024-09-20 16:25:08 +0300

    initial_commit9

--- src/main/java/com/calculatorAndTest/Test/calculator/exceptions/DivisionByZeroException.java
+++ src/main/java/com/calculatorAndTest/Test/calculator/exceptions/DivisionByZeroException.java
@@ -1,13 +1,13 @@
-      package com.calculatorAndTest.Test.calculator.exceptions;
+        package com.calculatorAndTest.Test.calculator.exceptions;
 
-      import org.springframework.http.HttpStatus;
-      import org.springframework.web.bind.annotation.ResponseStatus;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.web.bind.annotation.ResponseStatus;
 
-      @ResponseStatus(HttpStatus.BAD_REQUEST)
-      public class DivisionByZeroException extends IllegalArgumentException {
-          private static final String MESSAGE = "Делить на ноль нельзя";
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public class DivisionByZeroException extends IllegalArgumentException {
+            private static final String MESSAGE = "Делить на ноль нельзя";
 
-          public DivisionByZeroException() {
-              super(MESSAGE);
-          }
-      }
+            public DivisionByZeroException() {
+                super(MESSAGE);
+            }
+        }
