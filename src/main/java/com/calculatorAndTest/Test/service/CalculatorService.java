8d015d967ef3c60e0aa5c3c3e8641b425108b70c
Zver <SvetlanaEvcheko@yandex.ru>
2024-09-20 15:38:16 +0300

    initial commit7

--- src/main/java/com/calculatorAndTest/Test/service/CalculatorService.java
+++ src/main/java/com/calculatorAndTest/Test/service/CalculatorService.java
@@ -1,53 +1,53 @@
-    package com.calculatorAndTest.Test.service;
+      package com.calculatorAndTest.Test.service;
 
-    import com.calculatorAndTest.Test.calculator.exceptions.DivisionByZeroException;
-    import com.calculatorAndTest.Test.calculator.exceptions.InsufficientParamException;
-    import org.springframework.stereotype.Service;
+      import com.calculatorAndTest.Test.calculator.exceptions.DivisionByZeroException;
+      import com.calculatorAndTest.Test.calculator.exceptions.InsufficientParamException;
+      import org.springframework.stereotype.Service;
 
 
-    @Service
-    public class CalculatorService {
-        public String welcome() {
-            return "Welcome!";
-        }
+      @Service
+      public class CalculatorService {
+          public String welcome() {
+              return "Welcome!";
+          }
 
-        public String plus(Integer num1, Integer num2) {
-            if (num1 == null || num2 == null) {
-                throw new InsufficientParamException();
-            }
-            int result = num1 + num2;
-            return num1 + " + " + num2 + " = " + result;
-        }
-        public String minus(Integer num1, Integer num2) {
-            if (num1 == null || num2 == null) {
-                throw new InsufficientParamException();
-            }
-            int result = num1 - num2;
-            return num1 + " - " + num2 + " = " + result;
-        }
+          public String plus(Integer num1, Integer num2) {
+              if (num1 == null || num2 == null) {
+                  throw new InsufficientParamException();
+              }
+              int result = num1 + num2;
+              return num1 + " + " + num2 + " = " + result;
+          }
+          public String minus(Integer num1, Integer num2) {
+              if (num1 == null || num2 == null) {
+                  throw new InsufficientParamException();
+              }
+              int result = num1 - num2;
+              return num1 + " - " + num2 + " = " + result;
+          }
 
-        public String multiply(Integer num1, Integer num2) {
-            if (num1 == null || num2 == null) {
-                throw new InsufficientParamException();
-            }
-            int result = num1 * num2;
-            return num1 + " * " + num2 + " = " + result;
-        }
+          public String multiply(Integer num1, Integer num2) {
+              if (num1 == null || num2 == null) {
+                  throw new InsufficientParamException();
+              }
+              int result = num1 * num2;
+              return num1 + " * " + num2 + " = " + result;
+          }
 
-        public String divide(Integer num1, Integer num2) {
-            if (num1 == null || num2 == null) {
-                throw new InsufficientParamException();
-            }
-            if (num2 == 0) {
-                throw new DivisionByZeroException();
-            }
-            int result = num1 / num2;
-            int remainder = num1 % num2;
-            if (remainder == 0) {
-                return num1 + " / " + num2 + " = " + result;
-            }
-            else {
-                return num1 + " / " + num2 + " = " + result + " остаток: " + remainder;
-            }
-        }
-    }
+          public String divide(Integer num1, Integer num2) {
+              if (num1 == null || num2 == null) {
+                  throw new InsufficientParamException();
+              }
+              if (num2 == 0) {
+                  throw new DivisionByZeroException();
+              }
+              int result = num1 / num2;
+              int remainder = num1 % num2;
+              if (remainder == 0) {
+                  return num1 + " / " + num2 + " = " + result;
+              }
+              else {
+                  return num1 + " / " + num2 + " = " + result + " остаток: " + remainder;
+              }
+          }
+      }
