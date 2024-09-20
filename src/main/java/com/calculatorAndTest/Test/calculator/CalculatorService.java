d08a7db12ce1e623061751c5a50089bd75f5e2b2
Zver <SvetlanaEvcheko@yandex.ru>
2024-09-20 15:01:15 +0300

    initial commit3

--- src/main/java/com/calculatorAndTest/Test/calculator/CalculatorService.java
+++ src/main/java/com/calculatorAndTest/Test/calculator/CalculatorService.java
@@ -1,9 +1,9 @@
-package com.calculatorAndTest.Test.calculator;
+  package com.calculatorAndTest.Test.calculator;
 
-public interface CalculatorService {
-    String plus(Integer num1, Integer num2);
-    String minus(Integer num1, Integer num2);
-    String multiply(Integer num1, Integer num2);
-    String divide(Integer num1, Integer num2);
-}
+  public interface CalculatorService {
+      String plus(Integer num1, Integer num2);
+      String minus(Integer num1, Integer num2);
+      String multiply(Integer num1, Integer num2);
+      String divide(Integer num1, Integer num2);
+  }
 
